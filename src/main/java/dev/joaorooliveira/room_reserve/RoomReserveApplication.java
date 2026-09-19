802b9f4c37cb110f6c3611ea1135c50215599956
Joaorooliveira <joaovictorooliveira812@gmail.com>
2026-09-18 22:15:45 -0300

    first commit

---
+++ src/main/java/dev/joaorooliveira/room_reserve/RoomReserveApplication.java
@@ -0,0 +1,13 @@
+package dev.joaorooliveira.room_reserve;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoomReserveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoomReserveApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/joaorooliveira/room_reserve/RoomReserveApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.joaorooliveira.room_reserve;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoomReserveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
