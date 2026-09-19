4585e2101c63f82d0015e124e26db839e6eed357
Joaorooliveira <joaovictorooliveira812@gmail.com>
2026-09-18 23:31:27 -0300

    feat: adiciona classe funcionario

--- src/main/java/dev/joaorooliveira/room_reserve/RoomReserveApplication.java
+++ src/main/java/dev/joaorooliveira/room_reserve/RoomReserveApplication.java
@@ -2,8 +2,10 @@ package dev.joaorooliveira.room_reserve;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class RoomReserveApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/dev/joaorooliveira/room_reserve/domain/funcionario/Funcionario.java
@@ -0,0 +1,49 @@
+package dev.joaorooliveira.room_reserve.domain.funcionario;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Table(name = "funcionario")
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EntityListeners(AuditingEntityListener.class)
+public class Funcionario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 150)
+    private String nome;
+
+    @Column(nullable = false, length = 150, unique = true)
+    private String email;
+
+    @Column(nullable = false, length = 10)
+    private String ramal;
+
+    @CreatedDate
+    @Column(name = "criado_em", nullable = false, updatable = false)
+    private LocalDateTime criadoEm;
+
+    @LastModifiedDate
+    @Column(name = "atualizado_em", nullable = false)
+    private LocalDateTime atualizadoEm;
+}
