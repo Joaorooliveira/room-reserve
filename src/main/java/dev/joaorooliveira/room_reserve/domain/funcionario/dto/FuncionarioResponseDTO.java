1728d8159a741a9ed9550dfbe3098efb9ab30421
Joaorooliveira <joaovictorooliveira812@gmail.com>
2026-09-23 23:16:14 -0300

    feat: adiciona funcionario response dto

--- src/main/java/dev/joaorooliveira/room_reserve/domain/funcionario/dto/FuncionarioResponseDTO.java
+++ src/main/java/dev/joaorooliveira/room_reserve/domain/funcionario/dto/FuncionarioResponseDTO.java
@@ -1,4 +1,22 @@
 package dev.joaorooliveira.room_reserve.domain.funcionario.dto;
 
-public record FuncionarioResponseDTO() {
+import dev.joaorooliveira.room_reserve.domain.funcionario.Funcionario;
+
+public record FuncionarioResponseDTO(
+
+        Long id,
+        String nome,
+        String email,
+        String ramal
+) {
+
+    public static FuncionarioResponseDTO fromEntity(Funcionario funcionario) {
+        return new FuncionarioResponseDTO(
+                funcionario.getId(),
+                funcionario.getNome(),
+                funcionario.getEmail(),
+                funcionario.getRamal()
+        );
+    }
+
 }
