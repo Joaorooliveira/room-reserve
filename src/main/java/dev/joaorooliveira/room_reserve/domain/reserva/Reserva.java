15eeed9b8eb949a3e6d1e993388255c0e1a304c0
Joaorooliveira <joaovictorooliveira812@gmail.com>
2026-09-23 22:01:15 -0300

    feat: adiciona classe sala

--- src/main/java/dev/joaorooliveira/room_reserve/domain/reserva/Reserva.java
+++ src/main/java/dev/joaorooliveira/room_reserve/domain/reserva/Reserva.java
@@ -1,4 +1,60 @@
 package dev.joaorooliveira.room_reserve.domain.reserva;
 
+import dev.joaorooliveira.room_reserve.domain.funcionario.Funcionario;
+import dev.joaorooliveira.room_reserve.domain.reserva.enums.StatusReservaTipo;
+import dev.joaorooliveira.room_reserve.domain.sala.Sala;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+
+@Entity
+@Table(name = "reserva")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EntityListeners(AuditingEntityListener.class)
 public class Reserva {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private LocalDate data;
+
+    @Column(name = "hora_inicio",nullable = false)
+    private LocalTime horarioInicio;
+
+    @Column(name = "hora_fim",nullable = false)
+    private LocalTime horarioFim;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false,length = 20)
+    private StatusReservaTipo status;
+
+    @CreatedDate
+    @Column(name = "criado_em", nullable = false,updatable = false)
+    private LocalDateTime criadoEm;
+
+    @LastModifiedDate
+    @Column(name = "atualizado_em", nullable = false)
+    private LocalDateTime atualizadoEm;
+
+    @ManyToOne
+    @JoinColumn(name = "funcionario_id",nullable = false)
+    private Funcionario funcionario;
+
+    @ManyToOne
+    @JoinColumn(name = "sala_id",nullable = false)
+    private Sala sala;
 }
