5a55a13cc75b0c9aba491a3c2e036174934296e5
Joaorooliveira <joaovictorooliveira812@gmail.com>
2026-09-24 23:47:46 -0300

    feat: adiciona exception entidade nao encontrada

--- src/main/java/dev/joaorooliveira/room_reserve/infra/exception/EntidadeNaoEncontradaException.java
+++ src/main/java/dev/joaorooliveira/room_reserve/infra/exception/EntidadeNaoEncontradaException.java
@@ -1,4 +1,7 @@
 package dev.joaorooliveira.room_reserve.infra.exception;
 
-public class EntidadeNaoEncontradaException {
+public class EntidadeNaoEncontradaException extends RuntimeException {
+    public EntidadeNaoEncontradaException(String message) {
+        super(message);
+    }
 }
